41b9be55aa0584d3cd7ad389bced6bf3908a5e2d
Tony Wu <tonywu315@gmail.com>
2024-12-28 21:53:36 -0500

    Refactor

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  // Replace this with your code
+  return (
+    <div>
+      <h1>Spring 2025 Hack4Impact-UMD Technical Application Assessment</h1>
+    </div>
+  );
+};
+
+export default App;
